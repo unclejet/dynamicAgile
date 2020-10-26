a5e4244a45ea72d960eebc67b5737c5d38ecd5bf
UncleJet <hanjintian@eastedu.com>
2020-10-27 06:15:07 +0800

    feat(fizzbuzz-round1): 第一个周期：探测环境，根据探测结果决定进入下一个周期还是对环境进行修复

---
+++ fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.dynamic.agile.kata.fizzbuzz;
+
+import org.junit.Test;
+
+/**
+ * @author Uncle Jet(韩金田)
+ * @date 10/27/2020 5:35 AM
+ */
+public class FizzBuzzTest {
+    @Test
+    public void inspect_env() {
+
+    }
+}
