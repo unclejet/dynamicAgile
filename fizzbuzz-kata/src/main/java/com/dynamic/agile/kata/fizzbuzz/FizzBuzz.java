093015a3b03184ac4b216ec94f136dcef6276d27
UncleJet <hanjintian@eastedu.com>
2020-10-27 07:17:39 +0800

    feat(fizzbuzz-round1): 第一个周期，存在一个fizzBuzz系统可以和用户互动--red

---
+++ fizzbuzz-kata/src/main/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.dynamic.agile.kata.fizzbuzz;
+
+/**
+ * @author Uncle Jet(韩金田)
+ * @date 10/27/2020 7:16 AM
+ */
+public class FizzBuzz {
+}
--- fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzzTest.java
@@ -2,13 +2,17 @@ package com.dynamic.agile.kata.fizzbuzz;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 /**
  * @author Uncle Jet(韩金田)
  * @date 10/27/2020 5:35 AM
  */
 public class FizzBuzzTest {
     @Test
-    public void inspect_env() {
-
+    public void fizzBuzzSystem_exist() {
+        FizzBuzz fizzBuzz = null;
+        assertThat(fizzBuzz, notNullValue());
     }
 }
