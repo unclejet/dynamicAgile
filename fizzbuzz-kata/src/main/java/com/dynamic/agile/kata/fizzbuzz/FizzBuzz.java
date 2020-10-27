76a95842e07e2224eaa7794e293bb0199a09c1c6
UncleJet <hanjintian@eastedu.com>
2020-10-27 09:30:54 +0800

    feat(fizzbuzz-round1): 第一个周期，最小外部互动，起点：用户输入"1"，系统输出"1",快速满足用户的最小互动需求 -- refactoring，删除确认系统存在的场景，因为user input 1的互动场景可以涵盖删除的场景

--- fizzbuzz-kata/src/main/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzz.java
+++ fizzbuzz-kata/src/main/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,7 @@
 package com.dynamic.agile.kata.fizzbuzz;
 
+import java.util.List;
+
 /**
  * @author Uncle Jet(韩金田)
  * @date 10/27/2020 7:16 AM
--- fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,22 +1,15 @@
 package com.dynamic.agile.kata.fizzbuzz;
 
-import javafx.scene.web.HTMLEditor;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 /**
  * @author Uncle Jet(韩金田)
  * @date 10/27/2020 5:35 AM
  */
 public class FizzBuzzTest {
-    @Test
-    public void fizzBuzzSystem_exist() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz, notNullValue());
-    }
-
     @Test
     public void whenUserInput1_thenPrint1() {
         String userInput = "1";
