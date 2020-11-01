09f4e600bfd884b8785b177a25681a6699c09516
unclejet <unclejet@126.com>
2020-11-02 07:18:55 +0800

    feat(fizzbuzz-round1): 函数式编程，提升可读性

--- fizzbuzz-kata/src/main/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzz.java
+++ fizzbuzz-kata/src/main/java/com/dynamic/agile/kata/fizzbuzz/FizzBuzz.java
@@ -1,6 +1,7 @@
 package com.dynamic.agile.kata.fizzbuzz;
 
 import java.util.List;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,7 +12,11 @@ import java.util.stream.IntStream;
 public class FizzBuzz {
     public static List<String> getFizzBuzz(String userInput) {
         return IntStream.rangeClosed(1, Integer.parseInt(userInput))
-                .mapToObj(number -> number % 15 == 0 ? "fizzbuzz" : number % 3 == 0 ? "fizz" : number % 5 == 0 ? "buzz" : String.valueOf(number))
+                .mapToObj(doFizzBuzz())
                 .collect(Collectors.toList());
+    }
+
+    private static IntFunction<String> doFizzBuzz() {
+        return number -> number % 15 == 0 ? "fizzbuzz" : number % 3 == 0 ? "fizz" : number % 5 == 0 ? "buzz" : String.valueOf(number);
     }
 }
