116f81a4305c211b988e15dbc25a5116f836453c
unclejet <unclejet@126.com>
2020-11-04 06:15:23 +0800

    feat(fizzbuzz-modeling): 探测系统生长环境是否OK

---
+++ fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/modeling/FizzBuzzModelingTest.java
@@ -0,0 +1,17 @@
+package com.dynamic.agile.kata.fizzbuzz.modeling;
+
+import org.junit.Test;
+
+/**
+ * @author ：unclejet
+ * @date ：Created in 2020/11/4 6:13
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class FizzBuzzModelingTest {
+    @Test
+    public void systemGrowsEnvIsOk() {
+
+    }
+}
