53451b4440978e0958cb7d9a1e25d7e8e6259ab9
unclejet <unclejet@126.com>
2020-11-04 06:27:33 +0800

    feat(fizzbuzz-modeling): 系统出生周期，发现最简外部互动--用户输入1，得到fizzbuzz 1
    通过建模，发现最简单的用户互动是输入1得到1，而不是输入一个非法的字符，进行处理，理由：
    1.用户输入合法数字的场景比非法数字多，一般测试角色的用户会输入非法场景满足探测系统健壮性的互动需求
    2.输入非法数字牵扯正则表达式的校验，内部的复杂度也更高

---
+++ fizzbuzz-kata/src/main/java/com/dynamic/agile/kata/fizzbuzz/modeling/FizzBuzzModeling.java
@@ -0,0 +1,14 @@
+package com.dynamic.agile.kata.fizzbuzz.modeling;
+
+/**
+ * @author ：unclejet
+ * @date ：Created in 2020/11/4 6:20
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class FizzBuzzModeling {
+    public String getFizzBuzz(String s) {
+        return null;
+    }
+}
--- fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/modeling/FizzBuzzModelingTest.java
+++ fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/modeling/FizzBuzzModelingTest.java
@@ -2,6 +2,10 @@ package com.dynamic.agile.kata.fizzbuzz.modeling;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 /**
  * @author ：unclejet
  * @date ：Created in 2020/11/4 6:13
@@ -13,5 +17,17 @@ public class FizzBuzzModelingTest {
     @Test
     public void systemGrowsEnvIsOk() {
 
+    }
+
+    @Test
+    public void thereIsASystemExist() {
+        FizzBuzzModeling fizzBuzzModeling = new FizzBuzzModeling();
+        assertThat(fizzBuzzModeling, is(notNullValue()));
+    }
+
+    @Test
+    public void whenUserInput1_thenGet1() {
+        FizzBuzzModeling fizzBuzzModeling = new FizzBuzzModeling();
+        assertThat(fizzBuzzModeling.getFizzBuzz("1"), is("1"));
     }
 }
