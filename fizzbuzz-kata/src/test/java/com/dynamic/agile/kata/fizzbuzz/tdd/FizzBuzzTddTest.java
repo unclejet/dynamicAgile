2d82710a57b3045113516b2a6bfc0037f668aaa9
unclejet <unclejet@126.com>
2020-11-04 05:44:19 +0800

    feat(fizzbuzz-tdd): 探测测试环境是否OK

---
+++ fizzbuzz-kata/src/test/java/com/dynamic/agile/kata/fizzbuzz/tdd/FizzBuzzTddTest.java
@@ -0,0 +1,17 @@
+package com.dynamic.agile.kata.fizzbuzz.tdd;
+
+import org.junit.Test;
+
+/**
+ * @author ：unclejet
+ * @date ：Created in 2020/11/4 5:41
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class FizzBuzzTddTest {
+    @Test
+    public void nothing() {
+
+    }
+}
