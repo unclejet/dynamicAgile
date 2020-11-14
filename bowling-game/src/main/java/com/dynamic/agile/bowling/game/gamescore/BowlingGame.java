dd601cd52b207d75edcfaeb7030b497dac55c1c6
UncleJet <hanjintian@eastedu.com>
2020-11-14 09:52:32 +0800

    feat(bowling-game game score--TDD): 算分和roll方法做自己的事情--refactoring

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/BowlingGame.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/BowlingGame.java
@@ -1,5 +1,8 @@
 package com.dynamic.agile.bowling.game.gamescore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author UncleJet
  * @date 11/14/2020 9:09 AM
@@ -7,12 +10,16 @@ package com.dynamic.agile.bowling.game.gamescore;
  */
 public class BowlingGame {
     private int gameScore;
+    private List<Integer> rolls = new ArrayList<Integer>();
 
     public int getGameScore() {
+        for (Integer pins : rolls) {
+            gameScore += pins;
+        }
         return gameScore;
     }
 
     public void roll(int pins) {
-        gameScore += pins;
+        rolls.add(pins);
     }
 }
