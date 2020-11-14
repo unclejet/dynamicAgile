967dc7953caa634ea967036c1e70f80d7cd97714
UncleJet <hanjintian@eastedu.com>
2020-11-14 09:05:11 +0800

    feat(bowling-game game score--TDD): make sure environemnt is ok

---
+++ bowling-game/src/test/java/com/dynamic/agile/bowling/game/gamescore/BowlingGameScoreTest.java
@@ -0,0 +1,15 @@
+package com.dynamic.agile.bowling.game.gamescore;
+
+import org.junit.Test;
+
+/**
+ * @author UncleJet
+ * @date 11/14/2020 9:01 AM
+ * @description
+ */
+public class BowlingGameScoreTest {
+    @Test
+    public void nothing() {
+
+    }
+}
