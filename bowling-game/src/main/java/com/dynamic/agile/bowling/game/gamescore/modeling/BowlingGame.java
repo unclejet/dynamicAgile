574b38e2ec98f290d34aa06805f69b7a2f94e371
unclejet <unclejet@126.com>
2020-11-15 09:04:55 +0800

    feat(bowling-game game score modeling):case3 内部互动--局分等于所有10轮frame score之和，所以系统内部需要有frame的集合；算分以frame为单位，每个frame需要知道自己当前轮击倒的pins数目，所以需要有pins集合，记录每一投击倒的pins数目

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/modeling/BowlingGame.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/modeling/BowlingGame.java
@@ -1,15 +1,24 @@
 package com.dynamic.agile.bowling.game.gamescore.modeling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: UncleJet
  * @date: Created in 2020/11/15 7:37
  * @description:
  */
 public class BowlingGame {
+    public static final int MAX_ROLL_NUMBER = 21;
+    private int[] pins = new int[MAX_ROLL_NUMBER];
+    private int rollIndex;
+    private List<Frame> frames = new ArrayList<>();
+
     public int getGameScore() {
-        return 0;
+        return frames.stream().mapToInt(Frame::calculateScore).sum();
     }
 
     public void roll(int pins) {
+        this.pins[rollIndex++] = pins;
     }
 }
---
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/modeling/Frame.java
@@ -0,0 +1,12 @@
+package com.dynamic.agile.bowling.game.gamescore.modeling;
+
+/**
+ * @author: UncleJet
+ * @date: Created in 2020/11/15 8:57
+ * @description:
+ */
+public class Frame {
+    public int calculateScore() {
+        return 0;
+    }
+}
