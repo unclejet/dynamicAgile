be974cf06e80b689076d2318d0768dd84bf1e8b6
davidzhang33-da <wolf_linn@163.com>
2020-11-18 21:32:29 +0800

    V7 first test roll1 score0 pass

---
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/v7/BowlingGameV7.java
@@ -0,0 +1,10 @@
+package com.dynamic.agile.bowling.game.v7;
+
+public class BowlingGameV7 {
+    public void roll(int pins) {
+    }
+
+    public int score() {
+        return 0;
+    }
+}
---
+++ bowling-game/src/test/java/com/dynamic/agile/bowling/game/v7/BowlingGameV7Test.java
@@ -0,0 +1,55 @@
+package com.dynamic.agile.bowling.game.v7;
+
+import com.dynamic.agile.bowling.game.v6.BowlingGameV6;
+import com.dynamic.agile.bowling.game.v6.FrameV6;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author: DavidZhang33
+ * @date: Created in 2020/11/18 21：26
+ * @description:
+ */
+public class BowlingGameV7Test {
+    private BowlingGameV7 game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGameV7();
+    }
+
+    private void rollMany(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            roll(pins);
+        }
+    }
+
+    private void roll(int pins) {
+        game.roll(pins);
+    }
+
+    private void rollMiss(int firstPins, int secondPins) {
+        assert firstPins + secondPins < 10;
+        roll(firstPins);
+        roll(secondPins);
+    }
+
+    private void rollSpare(int firstPins, int secondPins) {
+        assert firstPins + secondPins == 10;
+        roll(firstPins);
+        roll(secondPins);
+    }
+
+    private void rollStrike() {
+        roll(10);
+    }
+
+    @Test
+    public void roll1(){
+        game.roll(1);
+        assertThat(game.score(),is(0));
+    }
+}
