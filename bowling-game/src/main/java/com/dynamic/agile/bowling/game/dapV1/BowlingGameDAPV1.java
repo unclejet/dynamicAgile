4955ef9968dd3485f809bfda4d2493515cd66b57
davidzhang33-da <wolf_linn@163.com>
2020-11-19 11:32:23 +0800

    外部互动：Roll1，Score0 内部互动：Frame如何得到HitPins，这里涉及到最关键的点：Frame跟Roll的关系 1. Frame摆放10个球瓶，Roll击倒球瓶，击倒的球瓶数就是Frame的hitPins 2. 外部互动不通过，重构Frame内的计分规则，只有存在两个hitPins才计分，否则本Frame0分

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/BowlingGameDAPV1.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/BowlingGameDAPV1.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 
 public class BowlingGameDAPV1 {
     private ArrayList<FrameDAPV1> frames = new ArrayList<>();
+    private FrameDAPV1 currentFrame;
+
+    public BowlingGameDAPV1() {
+        currentFrame = new FrameDAPV1();
+        frames.add(currentFrame);
+    }
+
     public void roll(int pins) {
+        currentFrame.hitPins(pins);
     }
 
     public int score() {
--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/FrameDAPV1.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/FrameDAPV1.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 
 class FrameDAPV1 {
     private ArrayList<Integer> hitPins = new ArrayList<>();
+
     int score() {
-        return hitPins.stream().mapToInt(Integer::intValue).sum();
+        return hitPins.size() == 2 ? hitPins.stream().mapToInt(Integer::intValue).sum() : 0;
+    }
+
+    void hitPins(int pins) {
+        hitPins.add(pins);
     }
 }
