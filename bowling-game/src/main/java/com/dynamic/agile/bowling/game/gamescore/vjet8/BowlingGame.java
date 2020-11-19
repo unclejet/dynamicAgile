9a7ec3940cc573fce04b39969a46e74ae3af9e62
unclejet <unclejet@126.com>
2020-11-19 15:03:46 +0800

    feat(bowling-game game score dynamic modeling): 内部互动1--现在聚焦到如何算局分：是十轮分数的累加

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/vjet8/BowlingGame.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/vjet8/BowlingGame.java
@@ -1,13 +1,17 @@
 package com.dynamic.agile.bowling.game.gamescore.vjet8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: UncleJet
  * @date: Created in 2020/11/19 14:40
  * @description:
  */
 public class BowlingGame {
+    private List<Frame> frames = new ArrayList<>();
     public int score() {
-        return -1;
+        return frames.stream().mapToInt(Frame::score).sum();
     }
 
     public void roll(int pins) {
---
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/gamescore/vjet8/Frame.java
@@ -0,0 +1,12 @@
+package com.dynamic.agile.bowling.game.gamescore.vjet8;
+
+/**
+ * @author: UncleJet
+ * @date: Created in 2020/11/19 14:54
+ * @description:
+ */
+public class Frame {
+    public int score() {
+        return 0;
+    }
+}
