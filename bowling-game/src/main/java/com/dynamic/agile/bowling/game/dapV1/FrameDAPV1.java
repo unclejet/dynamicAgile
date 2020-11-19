68877fe0695825f3eefba90fb6ef30368b8e2b55
davidzhang33-da <wolf_linn@163.com>
2020-11-19 11:21:26 +0800

    外部互动：Roll1，Score0————内部互动：Frame如何计分（在最简单业务的情况下），Frame的计分方式是击打球瓶数之和（hitPins）

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/FrameDAPV1.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/FrameDAPV1.java
@@ -1,7 +1,10 @@
 package com.dynamic.agile.bowling.game.dapV1;
 
+import java.util.ArrayList;
+
 class FrameDAPV1 {
+    private ArrayList<Integer> hitPins = new ArrayList<>();
     int score() {
-        return 0;
+        return hitPins.stream().mapToInt(Integer::intValue).sum();
     }
 }
