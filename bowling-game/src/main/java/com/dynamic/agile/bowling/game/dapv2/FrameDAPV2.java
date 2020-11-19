9e9a8d3818ab064d15652a030901a92dcf0e7d44
davidzhang33-da <wolf_linn@163.com>
2020-11-19 22:36:13 +0800

    Game系统逐渐浮现： 1. 外部互动：Roll，Score。 2. Game内部互动：Game-Frame-hitPins-Roll。 3. 处理Roll、Frame、HitPins的内部互动关系，Frame计分规则
    外部互动：Roll1，Score0 Pass；Roll11，Score2 Pass
    内部互动：Frame怎么计分？
    1. 第一次击球不计分，第二次击球开始计分，分数为两次击球数之和（默认Miss）

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapv2/FrameDAPV2.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapv2/FrameDAPV2.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 
 class FrameDAPV2 {
     private ArrayList<Integer> hitPins = new ArrayList<>();
+
     int score() {
-        return hitPins.stream().mapToInt(Integer::intValue).sum();
+        return hasTwoRolls() ? hitPins.get(0) + hitPins.get(1) : 0;
+    }
+
+    private boolean hasTwoRolls() {
+        return hitPins.size() == 2;
     }
 
     void hitPins(int pins) {
