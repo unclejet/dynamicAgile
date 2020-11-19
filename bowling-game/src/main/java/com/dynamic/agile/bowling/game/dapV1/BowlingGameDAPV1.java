e85f5d485a49be3aa7bd98c49b7914b1b805b6f8
davidzhang33-da <wolf_linn@163.com>
2020-11-19 11:08:23 +0800

    试着换个思路重新开始。————game.roll, game.score两个外部互动中，roll是最小周期。————TDD的第一个测试通过。

---
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV1/BowlingGameDAPV1.java
@@ -0,0 +1,10 @@
+package com.dynamic.agile.bowling.game.dapV1;
+
+public class BowlingGameDAPV1 {
+    public void roll(int pins) {
+    }
+
+    public int score() {
+        return 0;
+    }
+}
---
+++ bowling-game/src/test/java/com/dynamic/agile/bowling/game/dapV1/BowlingGameDAPV1Test.java
@@ -0,0 +1,50 @@
+package com.dynamic.agile.bowling.game.dapV1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class BowlingGameDAPV1Test {
+    private BowlingGameDAPV1 game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGameDAPV1();
+    }
+
+    private void rollMany(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            roll(pins);
+        }
+    }
+
+    private void roll(int pins) {
+        game.roll(pins);
+    }
+
+    private void rollMiss(int firstPins, int secondPins) {
+        assert firstPins + secondPins < 10;
+        roll(firstPins);
+        roll(secondPins);
+    }
+
+    private void rollSpare(int firstPins, int secondPins) {
+        assert firstPins + secondPins == 10;
+        roll(firstPins);
+        roll(secondPins);
+    }
+
+    private void rollStrike() {
+        roll(10);
+    }
+
+    @Test
+    public void roll1() {
+        roll(1);
+        assertThat(game.score(), is(0));
+    }
+
+}
