7857a206882d8e6eeaa2c998edba9cdf92c7f9aa
davidzhang33-da <wolf_linn@163.com>
2020-11-26 18:01:53 +0800

    确保最多十个Frame

--- bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV3/BowlingGameDAPV3.java
+++ bowling-game/src/main/java/com/dynamic/agile/bowling/game/dapV3/BowlingGameDAPV3.java
@@ -3,25 +3,45 @@ package com.dynamic.agile.bowling.game.dapV3;
 import java.util.ArrayList;
 
 class BowlingGameDAPV3 {
-    private ArrayList<FrameDAPV3> frames = new ArrayList<>();
+    private static final int FRAMES_OF_GAME = 10;
+    private ArrayList<FrameDAPV3> frames = new ArrayList<>(10);
     private ArrayList<Integer> rollPins = new ArrayList<>();
     private FrameDAPV3 currentFrame;
+
     BowlingGameDAPV3() {
         createNewFrame();
     }
 
     private void createNewFrame() {
-        currentFrame = new FrameDAPV3();
-        frames.add(currentFrame);
+        if (lessThan10Frames()) {
+            currentFrame = new FrameDAPV3();
+            frames.add(currentFrame);
+        }
     }
 
     void roll(int pins) {
         rollPins.add(pins);
+        if (lessThan10Frames()) {
+            frameRoll(pins);
+        } else if (alreadyHas10Frames() && !currentFrame.isFinish()) {
+            frameRoll(pins);
+        }
+    }
+
+    private void frameRoll(int pins) {
         currentFrame.hitPins(pins);
-        if(currentFrame.isFinish()){
+        if (currentFrame.isFinish()) {
             currentFrame.setRollPins(rollPins);
             createNewFrame();
         }
+    }
+
+    private boolean lessThan10Frames() {
+        return frames.size() < FRAMES_OF_GAME;
+    }
+
+    private boolean alreadyHas10Frames() {
+        return frames.size() == FRAMES_OF_GAME;
     }
 
     int score() {
