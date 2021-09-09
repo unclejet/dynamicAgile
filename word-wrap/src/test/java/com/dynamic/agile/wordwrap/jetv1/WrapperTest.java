dd23a3a27af0276411319b4fe7ea573a2bfa155b
unclejet <unclejet@126.com>
2021-09-09 12:14:22 +0800

    feat(v1 try)：2 char , 1 width ,green

--- word-wrap/src/test/java/com/dynamic/agile/wordwrap/jetv1/WrapperTest.java
+++ word-wrap/src/test/java/com/dynamic/agile/wordwrap/jetv1/WrapperTest.java
@@ -14,10 +14,13 @@ public class WrapperTest {
     @Test
     public void shouldWrap() {
         assertThat(wrap("x", 1), is("x"));
+        assertThat(wrap("xx", 2), is("xx"));
         assertThat(wrap("xx", 1), is("x\nx"));
     }
 
     private String wrap(String s, int width) {
-        return s;
+        if (s.length() <= width)
+            return s;
+        return s.substring(0, width) + "\n" + s.substring(width);
     }
 }
