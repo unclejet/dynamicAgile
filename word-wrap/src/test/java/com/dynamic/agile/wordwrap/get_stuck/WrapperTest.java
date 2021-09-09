c8d04d43622b5aa118cbbec9bca17133e63c3f89
unclejet <unclejet@126.com>
2021-09-09 11:54:24 +0800

    feat(get stuck)：get stuck

--- word-wrap/src/test/java/com/dynamic/agile/wordwrap/get_stuck/WrapperTest.java
+++ word-wrap/src/test/java/com/dynamic/agile/wordwrap/get_stuck/WrapperTest.java
@@ -15,6 +15,7 @@ public class WrapperTest {
     public void shouldWrap() {
         assertThat(wrap("word word", 4), is("word\nword"));
         assertThat(wrap("a dog", 5), is("a dog"));
+        assertThat(wrap("a dog with a bone", 6), is("a dog\nwith a\nbone"));
     }
 
     private String wrap(String s, int width) {
