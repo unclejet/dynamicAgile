f6b9d18340c7fe8806683453b6c462fe23270119
unclejet <unclejet@126.com>
2022-01-27 09:06:54 +0800

    feat(sort-bubble): green: sort 2 integer list.

--- sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
+++ sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
@@ -27,6 +27,13 @@ public class SortTest {
     }
 
     private List<Integer> sort(List<Integer> asList) {
+        if (asList.size() > 1) {
+            if (asList.get(0) > asList.get(1)) {
+                Integer temp = asList.get(0);
+                asList.set(0, asList.get(1));
+                asList.set(1, temp);
+            }
+        }
         return asList;
     }
 
