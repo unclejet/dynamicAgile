b6013a7d4dda5c9387f2b787d762f739f031aef0
unclejet <unclejet@126.com>
2022-01-27 08:38:16 +0800

    feat(sort-bubble): init project and test nothing.

---
+++ sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
@@ -0,0 +1,15 @@
+package com.dynamic.agile.bubblesort;
+
+import org.junit.Test;
+
+/**
+ * @author ：UncleJet
+ * @date ：Created in 2022/1/27 上午8:37
+ * @description：
+ */
+public class SortTest {
+    @Test
+    public void nothing() {
+
+    }
+}
