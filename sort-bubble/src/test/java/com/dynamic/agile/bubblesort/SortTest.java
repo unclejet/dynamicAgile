181ab4d65f9e193112919eec9c0f5a8869d4e0b1
unclejet <unclejet@126.com>
2022-01-27 10:04:03 +0800

    feat(sort-bubble): green: sort 3 integer list.

--- sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
+++ sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
@@ -22,6 +22,17 @@ public class SortTest {
         assertSorted(intList(2, 1), Arrays.asList(1, 2));
         assertSorted(intList(1, 3, 2), Arrays.asList(1, 2, 3));
         assertSorted(intList(3, 2, 1), Arrays.asList(1, 2, 3));
+        sortBigList(1000);
+    }
+
+    private void sortBigList(int n) {
+        List<Integer> unsorted = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            unsorted.add((int)(Math.random() * 10000.0));
+        }
+        List<Integer> sorted = sort(unsorted);
+        for (int i = 0; i < n - 1; i++)
+            assertThat(sorted.get(i) <= sorted.get(i + 1), is(true));
     }
 
     private void assertSorted(List<Integer> unsorted, List<Integer> sorted) {
