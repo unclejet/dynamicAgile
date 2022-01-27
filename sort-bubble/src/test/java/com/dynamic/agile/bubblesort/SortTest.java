971623b4fa33965ecc34a93247acb86a1ca28a77
unclejet <unclejet@126.com>
2022-01-27 08:44:12 +0800

    feat(sort-bubble): red: sort empty integer list.

--- sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
+++ sort-bubble/src/test/java/com/dynamic/agile/bubblesort/SortTest.java
@@ -2,6 +2,12 @@ package com.dynamic.agile.bubblesort;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * @author ：UncleJet
  * @date ：Created in 2022/1/27 上午8:37
@@ -9,7 +15,15 @@ import org.junit.Test;
  */
 public class SortTest {
     @Test
-    public void nothing() {
+    public void sortings() {
+        assertThat(sort(intList()), is(Arrays.asList()));
+    }
 
+    private String sort(List<Integer> asList) {
+        return null;
+    }
+
+    private List<Integer> intList() {
+        return Arrays.asList();
     }
 }
