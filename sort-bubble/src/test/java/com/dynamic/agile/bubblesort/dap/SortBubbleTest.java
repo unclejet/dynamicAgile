28dda3a8835c9fc8a50d1012238a78b43ed53f5c
hanjintian <hanjintian@cmii.chinamobile.com>
2022-01-28 11:21:31 +0800

    feat(sort-bubble-dap): init project and make sure UT can work!

---
+++ sort-bubble/src/test/java/com/dynamic/agile/bubblesort/dap/SortBubbleTest.java
@@ -0,0 +1,17 @@
+package com.dynamic.agile.bubblesort.dap;
+
+import org.junit.Test;
+
+/**
+ * @author ：unclejet
+ * @date ：Created in 2022/1/28 11:20
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class SortBubbleTest {
+    @Test
+    public void nothing() {
+
+    }
+}
