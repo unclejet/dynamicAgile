70c03e5196a91f047239515b496efab7c8071257
hanjintian <hanjintian@cmii.chinamobile.com>
2022-01-28 11:42:35 +0800

    feat(sort-bubble-dap): case1: sort empty integer list is easiest.

--- sort-bubble/src/test/java/com/dynamic/agile/bubblesort/dap/SortBubbleTest.java
+++ sort-bubble/src/test/java/com/dynamic/agile/bubblesort/dap/SortBubbleTest.java
@@ -2,6 +2,12 @@ package com.dynamic.agile.bubblesort.dap;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * @author ：unclejet
  * @date ：Created in 2022/1/28 11:20
@@ -11,7 +17,11 @@ import org.junit.Test;
  */
 public class SortBubbleTest {
     @Test
-    public void nothing() {
+    public void sortIntegerList() {
+        assertThat(sort(Arrays.asList()), is(Arrays.asList()));
+    }
 
+    private List<Integer> sort(List<Integer> asList) {
+        return asList;
     }
 }
