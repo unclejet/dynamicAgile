65b808b258838778a5b50e8312595ea9216833cc
unclejet <unclejet@126.com>
2022-05-23 12:42:24 +0800

    feat(list of 2): refactoring--make code more generic.

--- prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
+++ prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
@@ -23,7 +23,7 @@ public class PrimeFactorsTest {
 
     private List<Integer> of(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        if (n == 2) {
+        if (n > 1) {
             factors.add(2);
         }
         return factors;
