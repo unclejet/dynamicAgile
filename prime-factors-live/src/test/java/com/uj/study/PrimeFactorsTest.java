fc7b746f0f735161481f6cd52a627fc0da49e703
unclejet <unclejet@126.com>
2022-05-23 12:57:24 +0800

    feat(list of 6 and 7): pass

--- prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
+++ prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
@@ -22,6 +22,8 @@ public class PrimeFactorsTest {
         assertPrimeFactors(3, list(3));
         assertPrimeFactors(4, list(2, 2));
         assertPrimeFactors(5, list(5));
+        assertPrimeFactors(6, list(2, 3));
+        assertPrimeFactors(7, list(7));
     }
 
     private void assertPrimeFactors(int n, List<Integer> primeFactors) {
