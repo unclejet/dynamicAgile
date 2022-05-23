e936b9cefa21014c6f6809a62232be90b0a6cbdd
unclejet <unclejet@126.com>
2022-05-23 12:43:47 +0800

    feat(list of 3): green.

--- prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
+++ prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
@@ -25,7 +25,7 @@ public class PrimeFactorsTest {
     private List<Integer> of(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
         if (n > 1) {
-            factors.add(2);
+            factors.add(n);
         }
         return factors;
     }
