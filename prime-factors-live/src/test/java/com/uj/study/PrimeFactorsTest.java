9cc1961717dc1d4f711f898de1604f2c4f45004f
unclejet <unclejet@126.com>
2022-05-24 12:35:47 +0800

    feat(list of 9): refactor constant to variable.

--- prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
+++ prime-factors-live/src/test/java/com/uj/study/PrimeFactorsTest.java
@@ -34,18 +34,14 @@ public class PrimeFactorsTest {
 
     private List<Integer> of(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        if (n > 1) {
-            while (n % 2 == 0) {
-                factors.add(2);
-                n /= 2;
+        int divisor = 2;
+        while (n > 1) {
+            while (n % divisor == 0) {
+                factors.add(divisor);
+                n /= divisor;
             }
-            while (n % 3 == 0) {
-                factors.add(3);
-                n /= 3;
-            }
+            divisor++;
         }
-        if (n > 1)
-            factors.add(n);
         return factors;
     }
 
