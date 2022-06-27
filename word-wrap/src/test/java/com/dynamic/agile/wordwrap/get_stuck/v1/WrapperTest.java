4c8448760cfb486a488588ff3d6131162ce4a0d5
unclejet <unclejet@126.com>
2022-06-27 12:39:40 +0800

    feat(word-wrap get stuck v1): nothing.

---
+++ word-wrap/src/test/java/com/dynamic/agile/wordwrap/get_stuck/v1/WrapperTest.java
@@ -0,0 +1,15 @@
+package com.dynamic.agile.wordwrap.get_stuck.v1;
+
+import org.junit.Test;
+
+/**
+ * @author ：UncleJet
+ * @date ：Created in 2022/6/27 下午12:38
+ * @description：
+ */
+public class WrapperTest {
+    @Test
+    public void shouldWrap() {
+
+    }
+}
